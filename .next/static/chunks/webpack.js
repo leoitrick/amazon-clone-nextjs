f1bcbce2167b19fb50c3db97b8bd5e56b0d39f77
Leandro Moreira <leoitrick@Leandros-MacBook-Pro.local>
2021-08-06 17:07:53 -0400

    finished  products page

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -182,7 +182,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "11a476e165c01df9117a"; }
+/******/ 		__webpack_require__.h = function() { return "3bd1f670ec0fc2941c66"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
